8c54257e969707adff9659ed9bb973ca61c397d2
ffpfrn <mohammedfarhanfff>
2025-11-21 23:05:08 +0530

    feat: added Two Sum solution

---
+++ src/Leet217.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+public class Leet217 {
+    public static void main(String[] args) {
+
+        int[] nums = {1, 2, 3, 1};  // example input
+        boolean result = containsDuplicate(nums);
+
+        System.out.println("Contains Duplicate: " + result);
+    }
+
+    public static boolean containsDuplicate(int[] nums) {
+        HashSet<Integer> set = new HashSet<>();
+
+        for (int n : nums) {
+            if (set.contains(n)) {
+                return true; // duplicate found
+            }
+            set.add(n);
+        }
+        return false;
+    }
+}
